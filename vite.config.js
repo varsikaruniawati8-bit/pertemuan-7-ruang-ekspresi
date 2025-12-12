97bdba1b47c92743d5d23d39693bc09e05d6dc29
varsikaruniawati8-bit <varsikaruniawati8@gmail.com>
2025-12-12 10:44:16 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite"; // Plugin v4 baru
 
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
 })
